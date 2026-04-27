3e580731bf35adea2798b19b5e669afe11381fc1
kaahoza@gmail.com <kaahoza@gmail.com>
2026-04-27 12:15:01 +0200

    feat: Add Authentication

--- myproject/urls.py
+++ myproject/urls.py
@@ -16,8 +16,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework_simplejwt.views import  TokenRefreshView, TokenObtainPairView
+from accounts.views import RegisterView
 
 urlpatterns = [
+    #Blog API
     path('admin/', admin.site.urls),
-    path('api/blog/', include('blog.urls'))
+    path('api/blog/', include('blog.urls')),
+    
+    # Auth API
+    path('api/register', RegisterView.as_view()),
+    path('api/login',TokenObtainPairView.as_view()),
+    path('api/token/refresh/', TokenRefreshView.as_view()),
 ]
