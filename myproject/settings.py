46ec2a477856df7a3036fc116120e1e9e2473796
kaahoza@gmail.com <kaahoza@gmail.com>
2026-04-29 23:31:35 +0200

    fix: update settings

--- myproject/settings.py
+++ myproject/settings.py
@@ -26,11 +26,10 @@ SECRET_KEY = 'django-insecure-=z=22pqse*&s2gph1^!8yxetenyaa=59ad*-^8(i!1j@@cqmg3
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-<<<<<<< HEAD
+
 ALLOWED_HOSTS = ['django-fullstack-blog-application.onrender.com']
-=======
 # ALLOWED_HOSTS = ['localhost:3000']
->>>>>>> 32fbe59e903d95aba71e7c8c806e80035868e556
+
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:5173",
