dea6c3888e1f35513bdc04365fa311d1b932233f
kaahoza@gmail.com <kaahoza@gmail.com>
2026-05-01 20:26:18 +0200

    fix api url

--- frontend/src/api.jsx
+++ frontend/src/api.jsx
@@ -1,8 +1,19 @@
 import axios from "axios"
 
 const API = axios.create({
-    baseURL: "https://django-fullstack-blog-application-6.onrender.com/api/blog",
+    baseURL: "https://django-fullstack-blog-application-6.onrender.com/api/",
+    
+    
 })
+
+// const token = localStorage.getItem("access");
+
+// API.get("/", {
+//   headers: {
+//     Authorization: `Bearer ${token}`,
+//     'Content-Type': 'application/json'
+//   },
+// });
 
 export const setAuthToken = (Token) => {
     if(Token) {
