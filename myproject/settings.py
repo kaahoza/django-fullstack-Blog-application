03c1bf8685461a2e1f17d25f94a60e8fb033dcdd
kaahoza@gmail.com <kaahoza@gmail.com>
2026-05-01 16:25:48 +0200

    update settings.py

--- myproject/settings.py
+++ myproject/settings.py
@@ -93,7 +93,8 @@ import dj_database_url
 from decouple import config
 
 DATABASES = {
-    'default': dj_database_url.parse('postgresql://ivory_blog_db_user:nFKFgURX2blurQwFBEXzedKcK8ISLuuh@dpg-d7p897ho3t8c73av58c0-a/ivory_blog_db')
+    'default': dj_database_url.parse('postgresql://ivory_blog_db_user:nFKFgURX2blurQwFBEXzedKcK8ISLuuh@dpg-d7p897ho3t8c73av58c0-a/ivory_blog_db'),
+    'ENGINE': 'django.db.backends.postgresql'
 }
 # Password validation
 # https://docs.djangoproject.com/en/6.0/ref/settings/#auth-password-validators
